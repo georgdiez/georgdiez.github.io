13363d02ac6e1e8a5ff5a46152ca11931814ceca
georg <gega.diez@gmail.com>
2021-02-15 17:22:30 +0100

    new git

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Open Sans"', 'system-ui'],
+        serif: ['"Playfair Display"', 'system-ui'],
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
