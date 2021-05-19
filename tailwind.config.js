ba0962b1960406123fd1665028d7e8d90841cb2a
georgdiez <georg.diez@physik.uni-freiburg.de>
2021-05-19 11:53:31 +0200

    new starting page with sticky navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ module.exports = {
       fontFamily: {
         sans: ['"Open Sans"'],
         serif: ['"Playfair Display"'],
+      },
+      animation: {
+        bounce: "bounce 1s linear infinite",
+      },
+      keyframes: {
+        bounce: {
+          '0%, 100%': {
+            transform: 'translateY(-5%)',
+            animationTimingFunction: 'cubic-bezier(0.8,0,1,1)',
+          },
+          '50%': {
+            transform: 'none',
+            animationTimingFunction: 'cubic-bezier(0,0,0.2,1)',
+          },
+        },
       }
     },
   },
