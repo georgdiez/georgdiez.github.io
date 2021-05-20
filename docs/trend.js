178752c0a2a208d4ebf16de8b79c393f04e71469
georgdiez <georg.diez@physik.uni-freiburg.de>
2021-05-20 19:48:35 +0200

    run if in viewport

--- docs/trend.js
+++ docs/trend.js
@@ -1,3 +1,20 @@
-new Vue({
-  el: '#muenster'
+var inited = false
+
+function init() {
+  new Vue({
+    el: '#muenster'
+  })
+}
+
+$(window).on('scroll', function() {
+  if ( inited ) {
+    return
+  }
+  
+  if ( skyline.offsetTop >= window.innerHeight + document.body.scrollTop ) {
+    inited = true
+    init()
+  }
+  
 })
+
